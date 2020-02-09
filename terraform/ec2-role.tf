a1a453bbc15c3de0f383f705af693b9342055912
shgtkshruch <shgtk.shruch@gmail.com>
2020-02-09 22:21:53 +0900

    Attach ssm role to ec2 instance

--- terraform/ec2-role.tf
+++ terraform/ec2-role.tf
@@ -26,3 +26,8 @@ resource "aws_iam_role_policy_attachment" "ec2_role" {
   role       = aws_iam_role.ec2_role.name
   policy_arn = aws_iam_policy.ec2_role.arn
 }
+
+resource "aws_iam_instance_profile" "ec2" {
+  name = "ec2-profile"
+  role = aws_iam_role.ec2_role.name
+}
--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,7 +1,8 @@
 resource "aws_instance" "web" {
-  ami           = "ami-011facbea5ec0363b"
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.public.id
+  ami                  = "ami-011facbea5ec0363b"
+  instance_type        = "t2.micro"
+  subnet_id            = aws_subnet.public.id
+  iam_instance_profile = aws_iam_instance_profile.ec2.name
 
   tags = {
     Name = var.tag
