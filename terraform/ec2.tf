f48cbd9c0061670d049af8ddeaccea6c7fd6641f
shgtkshruch <shgtk.shruch@gmail.com>
2020-02-10 15:07:41 +0900

    Refactor

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -9,3 +9,36 @@ resource "aws_instance" "web" {
   }
 }
 
+resource "aws_iam_role" "ec2_role" {
+  name               = "SSMInstanceProfile"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "ec2_role" {
+  name   = "ssm_instance_profile_policy"
+  policy = templatefile("ec2-role-policy.tpl", {})
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_role" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.ec2_role.arn
+}
+
+resource "aws_iam_instance_profile" "ec2" {
+  name = "ec2-profile"
+  role = aws_iam_role.ec2_role.name
+}
